8866a064b6741e49b4c3863b74a1f550395712c7
Dongseon Lee <eastline@maycan.co.kr>
2021-08-13 21:36:55 +0900

    Merge: joy & woody

--- src/components/home/SectionCocktail.vue
+++ src/components/home/SectionCocktail.vue
@@ -1,21 +1,16 @@
 <template>
 	<div class="section section__cocktail">
 		<div class="full__container">
-			<span>혼술 레시피</span><br>
-			<div class = "cocktail__container">
-				<div 
-					class = "card__cocktail"
-					v-for="(cocktail, index) in cocktailRecipe"
-					:key="index"
-					avatar
-				>
+			<span>혼술 레시피</span><br />
+			<div class="cocktail__container">
+				<div class="card__cocktail" v-for="(cocktail, index) in cocktailRecipe" :key="index" avatar>
 					<div>
 						<!-- v-bind:src -->
-						<img :src = "cocktail.img"/>
-						<div class = "cocktail__content">
-							<p class = "cocktail__name">{{cocktail.cocktailName}}</p>
-							<hr/>
-							<p class = "cocktail__base">#{{cocktail.base}}</p>
+						<img :src="cocktail.img" />
+						<div class="cocktail__content">
+							<p class="cocktail__name">{{ cocktail.cocktailName }}</p>
+							<hr />
+							<p class="cocktail__base">#{{ cocktail.base }}</p>
 						</div>
 					</div>
 				</div>
@@ -27,76 +22,73 @@
 <style scoped>
 	span {
 		font-size: 36px;
-		border-bottom: 1px solid #B43539;
-		box-shadow: inset 0 -1px 0 #B43539;
+		border-bottom: 1px solid #b43539;
+		box-shadow: inset 0 -1px 0 #b43539;
 	}
-	.section__cocktail{
-		background-color: #EDEDED;
+	.section__cocktail {
+		background-color: #ededed;
 	}
-	.cocktail__container{
+	.cocktail__container {
 		padding-inline-start: 190px;
 		padding-inline-end: 190px;
 	}
-	.cocktail__container .card__cocktail{
-		display : inline-block; /* 한 줄로 정렬 */
+	.cocktail__container .card__cocktail {
+		display: inline-block; /* 한 줄로 정렬 */
 		width: 25%;
-		margin-top : 30px;
+		margin-top: 30px;
 		/* overflow: hidden; */
 	}
-	.cocktail__container .card__cocktail img{
+	.cocktail__container .card__cocktail img {
 		width: 200px;
 		height: 200px;
-		
-		-webkit-transform:scale(1);
-		-moz-transform:scale(1);
-		-ms-transform:scale(1); 
-		-o-transform:scale(1);  
-		transform:scale(1);
-		-webkit-transition:.3s;
-		-moz-transition:.3s;
-		-ms-transition:.3s;
-		-o-transition:.3s;
-		transition:.3s;
+
+		-webkit-transform: scale(1);
+		-moz-transform: scale(1);
+		-ms-transform: scale(1);
+		-o-transform: scale(1);
+		transform: scale(1);
+		-webkit-transition: 0.3s;
+		-moz-transition: 0.3s;
+		-ms-transition: 0.3s;
+		-o-transition: 0.3s;
+		transition: 0.3s;
 	}
 	.cocktail__container .card__cocktail:hover img {
-		-webkit-transform:scale(1.05);
-		-moz-transform:scale(1.05);
-		-ms-transform:scale(1.05);   
-		-o-transform:scale(1.05);
-		transform:scale(1.05);
+		-webkit-transform: scale(1.05);
+		-moz-transform: scale(1.05);
+		-ms-transform: scale(1.05);
+		-o-transform: scale(1.05);
+		transform: scale(1.05);
 	}
-	.cocktail__container .card__cocktail .cocktail__content{
+	.cocktail__container .card__cocktail .cocktail__content {
 		padding-inline-start: 10px;
 		padding-inline-end: 10px;
 	}
-	.cocktail__container .card__cocktail .cocktail__content hr{
-		padding : 0;
-		margin : 0;
+	.cocktail__container .card__cocktail .cocktail__content hr {
+		padding: 0;
+		margin: 0;
 		width: 30%;
-		margin-block-start : 10px;
-		margin-block-end : 10px;
+		margin-block-start: 10px;
+		margin-block-end: 10px;
 	}
-	.cocktail__container .card__cocktail .cocktail__content .cocktail__name{
+	.cocktail__container .card__cocktail .cocktail__content .cocktail__name {
 		font-size: 17px;
 		text-align: left;
-		margin-block-start : 16px;
+		margin-block-start: 16px;
 	}
-	.cocktail__container .card__cocktail .cocktail__content .cocktail__base{
+	.cocktail__container .card__cocktail .cocktail__content .cocktail__base {
 		font-size: 12px;
 		text-align: left;
 	}
-
 </style>
 
 <script>
-import { mapState } from 'vuex';
+	import { mapState } from "vuex";
 
 	export default {
 		name: "SectionCocktail",
-		computed : {
+		computed: {
 			...mapState(["cocktailRecipe"]),
-		}
+		},
 	};
 </script>
-
-
--- src/store/index.js
+++ src/store/index.js
@@ -4,21 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: { //data
+	state: {
+		//data
 
-    // 템플릿에서 <img :src="require(`@/assets/images/${imgName}.png`)"
-    cocktailRecipe : [
-      {cocktailName : '스크류키스', base : '소주', img : require('@/assets/images/스크류키스.png')},
-      {cocktailName : '에너자이저주', base : '소주', img : require('@/assets/images/에너자이저주.png')},
-      {cocktailName : '진토닉', base : '양주', img : require('@/assets/images/진토닉.png')},
-      {cocktailName : '키스헌터', base : '소주', img : require('@/assets/images/키스헌터.png')},
-      {cocktailName : '하이볼', base : '양주', img : require('@/assets/images/하이볼.png')},
-      {cocktailName : 'Old Fashioned', base : '양주', img : require('@/assets/images/Old_Fashioned.png')},
-      {cocktailName : 'Sherry Cobbler', base : '양주', img : require('@/assets/images/Sherry_Cobbler.png')},
-      {cocktailName : 'Tweak Classic', base : '양주', img : require('@/assets/images/Tweak_Classic.png')}
-    ]
-  },
-  mutations: {},
-  actions: {},
-  modules: {},
+		// 템플릿에서 <img :src="require(`@/assets/images/${imgName}.png`)"
+		cocktailRecipe: [
+			{ cocktailName: "스크류키스", base: "소주", img: require("@/assets/images/스크류키스.png") },
+			{ cocktailName: "에너자이저주", base: "소주", img: require("@/assets/images/에너자이저주.png") },
+			{ cocktailName: "진토닉", base: "양주", img: require("@/assets/images/진토닉.png") },
+			{ cocktailName: "키스헌터", base: "소주", img: require("@/assets/images/키스헌터.png") },
+			{ cocktailName: "하이볼", base: "양주", img: require("@/assets/images/하이볼.png") },
+			{ cocktailName: "Old Fashioned", base: "양주", img: require("@/assets/images/Old_Fashioned.png") },
+			{ cocktailName: "Sherry Cobbler", base: "양주", img: require("@/assets/images/Sherry_Cobbler.png") },
+			{ cocktailName: "Tweak Classic", base: "양주", img: require("@/assets/images/Tweak_Classic.png") },
+		],
+	},
+	mutations: {},
+	actions: {},
+	modules: {},
 });
