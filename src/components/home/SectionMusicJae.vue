05c65665d29c40f2b9392935d9737495a7aea8c5
DESKTOP-QPICQ5M\cocoa <cocoa389@naver.com>
2021-08-17 20:43:05 +0900

    Second Commit

--- src/components/home/SectionMusicJae.vue
+++ src/components/home/SectionMusicJae.vue
@@ -5,11 +5,14 @@
 			<div class="music__wrapper">
 				<div class="music__box">
 					<div class="music_inform" style="color: white" v-for="(music, index) in musicList" :key="index">
-						<img :src="music.img" />
+						<div class="thumbnail">
+							<img :src="music.img" />
+							<p class="thumbnail__title">{{ music.thumbnail }}</p>
+						</div>
 						<p class="music__title">{{ music.musicTitle }}</p>
-						<p class="DJ__name">#{{ music.DJName }}</p>
+						<p class="DJ__name">{{ music.DJName }}</p>
+						<p class="go_honsuri">혼술하러 가기 ></p>
 					</div>
-					<p>혼술하러가기</p>
 				</div>
 			</div>
 		</div>
@@ -17,31 +20,71 @@
 </template>
 
 <style scoped>
-	.section_music {
-		position: relative;
-		background: black;
-	}
-
 	.full__container h1 {
 		font-size: 48px;
-		font-weight: 300;
-		margin-top: 113px;
+		font-weight: 400;
 		margin-bottom: 50px;
+		margin-top: 113px;
 	}
 
 	.music__wrapper {
-		margin-left: -120px;
-		width: 1560px;
+		margin: 0px -120px;
 		height: 612px;
 		background: black;
-	}
-	.music__box img {
-		width: 290px;
-		padding: 20px;
+		margin-bottom: 230px;
+		padding-left: 130px;
 	}
 
+	.music__box {
+		width: 290px;
+		height: 322+290px;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 40px;
+		display: flex;
+	}
+	.thumbnail {
+		margin-top: 60px;
+		width: 290px;
+		height: 290px;
+		position: relative;
+	}
+	.music_inform {
+		margin-top: 20px;
+		width: 290px;
+		position: relative;
+	}
 	.music_inform p {
-		padding: 20px;
+		text-align: left;
+	}
+	.music__title {
+		font-weight: 500;
+		font-size: 20px;
+		padding: 20px 0px;
+		width: 290px;
+		height: 58px;
+	}
+	.thumbnail__title {
+		display: flex;
+		align-items: center;
+		word-break: keep-all;
+		font-size: 50px;
+		color: white;
+		font-weight: 600;
+		width: 230px;
+		height: 100px;
+		transform: translate(15%, -200%);
+		text-align: center;
+		z-index: 1;
+	}
+	.DJ__name {
+		font-weight: 200;
+		padding-bottom: 50px;
+		font-size: 18px;
+	}
+	.go_honsuri {
+		color: gray;
+		text-decoration: underline;
 	}
 </style>
 
@@ -55,21 +98,25 @@
 						musicTitle: "혼술 먹방에 딱맞는 분위기 깡패 혼술 음악",
 						DJName: "리플레이 LEEPLAY",
 						img: require("@/assets/images/relaxing_piano.png"),
+						thumbnail: "RELAXING PIANO",
 					},
 					{
 						musicTitle: "새벽 혼술 최고의 술안주",
 						DJName: "MAGIC STRAWBERRY",
 						img: require("@/assets/images/groovy_mix.png"),
+						thumbnail: "GROOVY R&B MIX",
 					},
 					{
 						musicTitle: "혼자 있을 때 듣는 감성적인 음악",
 						DJName: "미러볼 뮤직",
 						img: require("@/assets/images/pop.png"),
+						thumbnail: "POP",
 					},
 					{
 						musicTitle: "반짝이는 야경과 함께 느긋하게 즐기는 밤 재즈",
 						DJName: "우든체어",
 						img: require("@/assets/images/lofi-jazz.png"),
+						thumbnail: "LO-FI JAZZ",
 					},
 				],
 			};
