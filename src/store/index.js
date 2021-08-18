ec6ba42cf3293a0b202664f3b70b4e88efc41055
DESKTOP-QPICQ5M\cocoa <cocoa389@naver.com>
2021-08-19 03:39:51 +0900

    Solve Scroll Lock & Remember Problem

--- src/store/index.js
+++ src/store/index.js
@@ -5,65 +5,23 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		//data
-		// SectionCocktail Vue
-		cocktailRecipe: [
-			{
-				cocktailName: "스크류키스",
-				base: "소주",
-				img: require("@/assets/images/스크류키스.png"),
-			},
-			{
-				cocktailName: "에너자이저주",
-				base: "소주",
-				img: require("@/assets/images/에너자이저주.png"),
-			},
-			{
-				cocktailName: "진토닉",
-				base: "양주",
-				img: require("@/assets/images/진토닉.png"),
-			},
-			{
-				cocktailName: "키스헌터",
-				base: "소주",
-				img: require("@/assets/images/키스헌터.png"),
-			},
-			{
-				cocktailName: "하이볼",
-				base: "양주",
-				img: require("@/assets/images/하이볼.png"),
-			},
-			{
-				cocktailName: "Old Fashioned",
-				base: "양주",
-				img: require("@/assets/images/Old_Fashioned.png"),
-			},
-			{
-				cocktailName: "Sherry Cobbler",
-				base: "양주",
-				img: require("@/assets/images/Sherry_Cobbler.png"),
-			},
-			{
-				cocktailName: "Tweak Classic",
-				base: "양주",
-				img: require("@/assets/images/Tweak_Classic.png"),
-			},
-		],
-
 		// LandingIntrance Vue
 		isRemember: false,
 	},
 	mutations: {
-		// btnDisable(state){
-		// 	state.intranceBtnActive = false;
-		// },
-		// btnAble(state){
-		// 	state.intranceBtnActive = true;
-		// },
-		// intranceSuccess(state){
-		// 	state.intranceContainerDisable = false
-		// }
+		rememberChecked(state) {
+			state.isRemember = !state.isRemember;
+			if (state.isRemember == true) {
+				localStorage.setItem("remember", true);
+			} else {
+				localStorage.removeItem("remember");
+			}
+		},
 	},
-	actions: {},
+	actions: {
+		rememberCheck({ commit }) {
+			commit("rememberChecked");
+		},
+	},
 	modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="home">
-		<LandingIntranceVue :value="IsClosedLanding" @input="DisableLanding" />
+		<LandingIntranceVue :value="[IsClosedLanding, IsRemebered]" @input="[DisableLanding, CheckRemember]" />
 		<full-page ref="fullpage" :options="options" id="fullpage">
 			<SectionMainVue />
 			<!-- components/home/ 위치에 컴포넌트화 시켜서 본인 섹션 구성하기!!!
@@ -8,14 +8,36 @@
 			- Default Setting은 SectionIntroductionVue 참고
 			 -->
 			<!-- IsClosedLanding이 참일 경우 아래도 보여주기!-->
-			<SectionIntroductionVue v-show="IsClosedLanding" />
-			<SectionMusicJae v-show="IsClosedLanding" />
-			<SectionCocktailVue v-show="IsClosedLanding" />
+			<SectionIntroductionVue v-show="IsClosedLanding || CheckRemember" />
+			<SectionMusicJae v-show="IsClosedLanding || CheckRemember" />
+			<SectionCocktailVue v-show="IsClosedLanding || CheckRemember" />
 			<!-- SectionBoard Elena -->
 		</full-page>
 	</div>
 </template>
-
+<style>
+	.fp-right {
+		position: fixed;
+		color: #ffff;
+		right: 5%;
+		top: 50%;
+		font-size: 24px;
+		transform: translateY(-50%);
+		margin: 0 auto;
+	}
+	.fp-right ul li:not(:first-child, :last-child) {
+		display: none;
+	}
+	.fp-right ul li:first-child::after {
+		content: "";
+		display: block;
+		width: 4px;
+		border-radius: 2px;
+		background: white;
+		height: 200px;
+		margin: 10px auto;
+	}
+</style>
 <script>
 	// @ is an alias to /src
 	import SectionMainVue from "../components/home/SectionMain.vue";
@@ -29,11 +51,15 @@
 		data: function () {
 			return {
 				IsClosedLanding: false,
+				IsRemembered: false,
 			};
 		},
 		methods: {
 			DisableLanding(val) {
 				this.IsClosedLanding = val;
+			},
+			CheckRemember(val) {
+				this.IsRemembered = val;
 			},
 		},
 		components: {
@@ -45,6 +71,10 @@
 		},
 		options: {
 			licenseKey: "YOUR_KEY_HEERE",
+			anchors: ["01", "02", "03", "04"],
+			navigation: true,
+			scrollOverflow: true,
+			scrollBar: false,
 		},
 	};
 </script>
