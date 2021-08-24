96a6e1e994d74ba4ba0d7ccc1341be4d8b71a2c9
Dongseon Lee <eastline@maycan.co.kr>
2021-08-24 11:17:29 +0900

    Refactoring: store

--- src/store/index.js
+++ src/store/index.js
@@ -4,23 +4,8 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		isRemember: false,
-	},
-	mutations: {
-		rememberChecked(state) {
-			state.isRemember = !state.isRemember;
-			if (state.isRemember == true) {
-				localStorage.setItem("remember", true);
-			} else {
-				localStorage.removeItem("remember");
-			}
-		},
-	},
-	actions: {
-		rememberCheck({ commit }) {
-			commit("rememberChecked");
-		},
-	},
+	state: {},
+	mutations: {},
+	actions: {},
 	modules: {},
 });
