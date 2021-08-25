ad42ce8c3c9605a9dcabd907a360c53ef1980286
chajuhui123 <jjuhee0913@gmail.com>
2021-08-25 21:15:15 +0900

    Feat : Add Conversation Router

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ "../views/Recipes.vue"),
 	},
+	{
+		path: "/conversation",
+		name: "Conversation",
+		component: () => import(/* webpackChunkName: "conversation" */ "../views/Conversation.vue"),
+	},
 ];
 
 const router = new VueRouter({
---
+++ src/views/Conversation.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="recipes">
+		<TheHeaderVue />
+	</div>
+</template>
+
+<script>
+	import TheHeaderVue from "../components/TheHeader.vue";
+
+	export default {
+		name: "Conversation",
+		components: {
+			TheHeaderVue,
+		},
+		data() {
+			return {};
+		},
+	};
+</script>
